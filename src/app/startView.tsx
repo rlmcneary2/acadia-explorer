37415e15b757e1481026a52f65a9f25976110f5c
Rich McNeary <rlmcneary2@gmail.com>
2017-08-27 17:37:56 -0400

    Added react-intl.

--- src/app/startView.tsx
+++ src/app/startView.tsx
@@ -1,17 +1,26 @@
 
 
-import { Provider } from "react-redux";
 import * as React from "react";
+import { IntlProvider } from "react-intl";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import App from "./view/app";
 
 
 export default ({ store }) => {
+    const locale = "en-US";
+    const intlProps = {
+        key: locale, // Set the key value to get React to re-render when the locale changes.
+        locale
+    };
+
     return (
         <Provider store={store}>
-            <Router>
-                <Route path="/" component={App} />
-            </Router>
+            <IntlProvider {...intlProps}>
+                <Router>
+                    <Route path="/" component={App} />
+                </Router>
+            </IntlProvider>
         </Provider>
     );
 };
