7d9b3e6de8a4c260001ad9fe5027105beaf94fca
Rich McNeary <rlmcneary2@gmail.com>
2017-08-28 22:46:17 -0400

    Service worker now caches results.

--- src/app/serviceWorker/serviceWorker.ts
+++ src/app/serviceWorker/serviceWorker.ts
@@ -4,6 +4,47 @@
 // the service worker functions directly (see addEventListener).
 
 
-addEventListener("install", function (evt) {
-    // TODO: install steps.
+const _CACHE_VERSION = "v1";
+
+
+addEventListener("activate", function (evt) {
+    // Delete old caches here.
 });
+
+addEventListener("fetch", async function (evt) {
+    const e = evt as any;
+    e.respondWith(getFromCacheOrFetch(e));
+});
+
+addEventListener("install", function (evt) {
+    (evt as any).waitUntil(
+        caches.open(_CACHE_VERSION)
+            .then(cache => {
+                return cache.addAll([
+                    // File path strings of resources here.
+                    "/index.html",
+                    "/app.js"
+                ]);
+            })
+    );
+});
+
+
+async function getFromCacheOrFetch(evt: any): Promise<any> {
+    let r = await caches.match(evt.request);
+    if (!r) {
+        r = await fetch(evt.request);
+
+        // We may not want to cache everything. Like requests for the current
+        // status of Island Explorer locations.
+
+        await putInCache(evt, r);
+    }
+
+    return r;
+}
+
+async function putInCache(evt: any, response: any) {
+    const cache = await caches.open(_CACHE_VERSION);
+    cache.put(evt.request, response.clone());
+}
