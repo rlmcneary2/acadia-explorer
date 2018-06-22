e6d76a151147cd53b5d9031dbf51e4db5f0128a1
Rich McNeary <rlmcneary2@gmail.com>
2018-06-22 18:04:18 -0400

    Comment out redux log.

--- src/app/start.ts
+++ src/app/start.ts
@@ -24,7 +24,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as redux from "redux";
-import { createLogger } from "redux-logger";
+// import { createLogger } from "redux-logger";
 import reduxThunk from "redux-thunk";
 import { actionApp } from "./action/app";
 import reducers from "./reducer/reducers";
@@ -41,22 +41,22 @@ export default async () => {
     const middlewareArgs = [reduxThunk];
 
     // Should not be in production.
-    const reduxLog = createLogger({
-        colors: {
-            action: () => "blue",
-            error: () => "blue",
-            nextState: () => "blue",
-            prevState: () => "blue",
-            title: () => "blue"
-        },
-        level: {
-            action: () => "log",
-            error: () => "log",
-            nextState: () => "log",
-            prevState: () => "log"
-        }
-    });
-    middlewareArgs.push(reduxLog);
+    // const reduxLog = createLogger({
+    //     colors: {
+    //         action: () => "blue",
+    //         error: () => "blue",
+    //         nextState: () => "blue",
+    //         prevState: () => "blue",
+    //         title: () => "blue"
+    //     },
+    //     level: {
+    //         action: () => "log",
+    //         error: () => "log",
+    //         nextState: () => "log",
+    //         prevState: () => "log"
+    //     }
+    // });
+    // middlewareArgs.push(reduxLog);
 
     const reduxMiddleware = redux.applyMiddleware(...middlewareArgs);
     const store = redux.createStore(reducers, {}, reduxMiddleware);
